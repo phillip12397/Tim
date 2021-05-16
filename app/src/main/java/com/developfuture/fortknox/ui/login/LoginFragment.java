99083d84e04537df6b8ec2aefc8297ed471afedc
Percy Mummelthey <s60418@beuth-hochschule.de>
2021-05-16 14:02:51 +0200

    FK-54 update navigation function

---
+++ app/src/main/java/com/developfuture/fortknox/ui/login/LoginFragment.java
@@ -0,0 +1,61 @@
+package com.developfuture.fortknox.ui.login;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.developfuture.fortknox.R;
+import com.developfuture.fortknox.ui.home.HomeViewModel;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * create an instance of this fragment.
+ */
+public class LoginFragment extends Fragment {
+
+    private LoginViewModel loginViewModel;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        loginViewModel = new ViewModelProvider(this).get(LoginViewModel.class);
+
+        View root = inflater.inflate(R.layout.fragment_login, container, false);
+
+        final TextView emailEntry = root.findViewById(R.id.editTextTextEmailAddress);
+        final Button loginButton = root.findViewById(R.id.login_button);
+
+        loginViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+
+            @Override
+            public void onChanged(@Nullable String string) {
+                emailEntry.setText(string);
+            }
+        });
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Navigation.findNavController(root).navigate(R.id.navigateLoginToHome);
+            }
+        });
+        return root;
+    }
+}
