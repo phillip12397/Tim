422dbb7149df8559dcd1e6736cb65e80771d6357
phillip <lip12397@gmail.com>
2021-05-19 00:32:04 +0200

    FK-25 made shortkey popup, but it is not connected yet

--- app/src/main/java/com/developfuture/fortknox/ui/home/HomeFragment.java
+++ app/src/main/java/com/developfuture/fortknox/ui/home/HomeFragment.java
@@ -1,11 +1,11 @@
 package com.developfuture.fortknox.ui.home;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
