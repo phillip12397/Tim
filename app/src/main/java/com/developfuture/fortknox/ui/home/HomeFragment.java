f24d45a814aa683b526c93bf76e06c4e423cc85c
phillip <lip12397@gmail.com>
2021-05-21 12:15:59 +0200

    FK-25 edit popup

--- app/src/main/java/com/developfuture/fortknox/ui/home/HomeFragment.java
+++ app/src/main/java/com/developfuture/fortknox/ui/home/HomeFragment.java
@@ -1,19 +1,28 @@
 package com.developfuture.fortknox.ui.home;
 
 import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.developfuture.fortknox.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class HomeFragment extends Fragment implements View.OnClickListener, View.OnLongClickListener {
+public class HomeFragment extends Fragment implements View.OnClickListener, View.OnLongClickListener{
 
     private HomeViewModel homeViewModel;
 
@@ -35,6 +44,58 @@ public class HomeFragment extends Fragment implements View.OnClickListener, View
         final ImageButton imageButton5 = root.findViewById(R.id.imageButton5);
         //final ImageButton imageButton6 = root.findViewById(R.id.imageButton6);
 
+        final FloatingActionButton detailedAdd = (FloatingActionButton) root.findViewById(R.id.fab);
+
+        FloatingActionButton fab = root.findViewById(R.id.fab);
+        fab.setOnClickListener(view -> {
+
+        });
+
+        detailedAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                final Dialog fbDialogue = new Dialog(getActivity(), android.R.style.Theme_Black_NoTitleBar);
+                fbDialogue.getWindow().setBackgroundDrawable(new ColorDrawable(Color.argb(100, 0, 0, 0)));
+                fbDialogue.setContentView(R.layout.add_transaction);
+                fbDialogue.setCanceledOnTouchOutside(true);
+                fbDialogue.show();
+
+                Spinner spinner = fbDialogue.findViewById(R.id.addTransactionSpinner);
+                ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(), R.array.add_transaction_names, android.R.layout.simple_spinner_item);
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                spinner.setAdapter(adapter);
+                spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                        String text = parent.getItemAtPosition(position).toString();
+                        Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> parent) {
+
+                    }
+                });
+                final ImageButton closeButton = fbDialogue.findViewById(R.id.addTransactionClose);
+
+                closeButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        fbDialogue.dismiss();
+                    }
+                });
+            }
+        });
+
+        /*
+            Spinner spinner = findViewById(R.id.addTransactionSpinner);
+            ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.add_transaction_names, android.R.layout.simple_spinner_item);
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            spinner.setAdapter(adapter);
+            spinner.setOnItemSelectedListener(this);
+         */
+
         imageButton1.setOnLongClickListener(this);
         imageButton2.setOnLongClickListener(this);
         imageButton3.setOnLongClickListener(this);
@@ -55,20 +116,48 @@ public class HomeFragment extends Fragment implements View.OnClickListener, View
     @Override
     public void onClick(View v) {
         int id = v.getId();
+        final Dialog fbDialogue = new Dialog(getActivity(), android.R.style.Theme_Black_NoTitleBar);
+        fbDialogue.getWindow().setBackgroundDrawable(new ColorDrawable(Color.argb(100, 0, 0, 0)));
+        fbDialogue.setContentView(R.layout.popup_home_shortkeys);
+        fbDialogue.setCanceledOnTouchOutside(true);
+        fbDialogue.show();
+
+        final TextView textView = fbDialogue.findViewById(R.id.shortkeyID);
+        final Button add =fbDialogue.findViewById(R.id.shortkeyAdd);
+        final ImageButton closeButton = fbDialogue.findViewById(R.id.shortkeyClose);
+        closeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fbDialogue.dismiss();
+            }
+        });
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fbDialogue.dismiss();
+            }
+        });
+
         switch (id){
             case R.id.imageButton1:
+                textView.setText("Food");
                 System.out.println("test1");
                 break;
             case R.id.imageButton2:
+                textView.setText("Games");
                 System.out.println("test2");
                 break;
             case R.id.imageButton3:
+                textView.setText("Gas Station");
                 System.out.println("test3");
                 break;
             case R.id.imageButton4:
+                textView.setText("Party");
                 System.out.println("test4");
                 break;
             case R.id.imageButton5:
+                textView.setText("Birthday");
                 System.out.println("test5");
                 break;
             //case R.id.imageButton6:
@@ -82,6 +171,22 @@ public class HomeFragment extends Fragment implements View.OnClickListener, View
     @Override
     public boolean onLongClick(View v) {
         int id = v.getId();
+
+        final Dialog fbDialogue = new Dialog(getActivity(), android.R.style.Theme_Black_NoTitleBar);
+        fbDialogue.getWindow().setBackgroundDrawable(new ColorDrawable(Color.argb(100, 0, 0, 0)));
+        fbDialogue.setContentView(R.layout.change_icon);
+        fbDialogue.setCanceledOnTouchOutside(true);
+        fbDialogue.show();
+
+        final TextView textView = fbDialogue.findViewById(R.id.shortkeyID);
+        final ImageButton closeButton = fbDialogue.findViewById(R.id.shortkeyCloseIcons);
+        closeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fbDialogue.dismiss();
+            }
+        });
+
         switch (id){
             case R.id.imageButton1:
                 System.out.println("test1 Hold");
