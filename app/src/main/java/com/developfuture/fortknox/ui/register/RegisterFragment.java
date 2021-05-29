9d4bc29e939740f07fc0fd3b334123bfd235d735
Percy Mummelthey <s60418@beuth-hochschule.de>
2021-05-29 15:55:23 +0200

    FK-58 add logic for login and register with firebase backend project

--- app/src/main/java/com/developfuture/fortknox/ui/register/RegisterFragment.java
+++ app/src/main/java/com/developfuture/fortknox/ui/register/RegisterFragment.java
@@ -9,14 +9,22 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.developfuture.fortknox.R;
-import com.developfuture.fortknox.ui.login.LoginViewModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import static android.content.ContentValues.TAG;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -24,7 +32,9 @@ import com.developfuture.fortknox.ui.login.LoginViewModel;
  */
 public class RegisterFragment extends Fragment {
 
+    private View root = null;
     private RegisterViewModel loginViewModel;
+    private FirebaseAuth mAuth;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -35,11 +45,13 @@ public class RegisterFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         loginViewModel = new ViewModelProvider(this).get(RegisterViewModel.class);
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+        root = inflater.inflate(R.layout.fragment_register, container, false);
 
-        View root = inflater.inflate(R.layout.fragment_register, container, false);
-
-        final TextView emailEntry = root.findViewById(R.id.editTextTextEmailAddress);
-        final Button loginButton = root.findViewById(R.id.register_button);
+        final TextView emailEntry = root.findViewById(R.id.editTextEmailAddress);
+        final TextView passwordEntry = root.findViewById(R.id.editTextPassword);
+        final Button registerButton = root.findViewById(R.id.register_button);
         final TextView tertyaryLoginButton = root.findViewById(R.id.tertiaryLoginButton);
 
         loginViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
@@ -50,11 +62,14 @@ public class RegisterFragment extends Fragment {
             }
         });
 
-        loginButton.setOnClickListener(new View.OnClickListener() {
+        registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                Navigation.findNavController(root).navigate(R.id.navigationRegisterToHome);
+                String email = emailEntry.getText().toString();
+                String password = passwordEntry.getText().toString();
+
+                createAccount(email, password);
             }
         });
 
@@ -66,5 +81,49 @@ public class RegisterFragment extends Fragment {
         });
 
         return root;
+    }
+
+    private void createAccount(String email, String password) {
+        // [START create_user_with_email]
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                            Navigation.findNavController(root).navigate(R.id.navigationRegisterToHome);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(getActivity(), "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                    }
+                });
+        // [END create_user_with_email]
+    }
+
+    private void sendEmailVerification() {
+        // Send verification email
+        // [START send_email_verification]
+        final FirebaseUser user = mAuth.getCurrentUser();
+        user.sendEmailVerification()
+                .addOnCompleteListener(getActivity(), new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        // Email sent
+                    }
+                });
+        // [END send_email_verification]
+    }
+
+    private void reload() { }
+
+    private void updateUI(FirebaseUser user) {
+
     }
 }
